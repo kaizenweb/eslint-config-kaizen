6c78f19d35959a4a23efd5a4386a87a5a3be18ec
Sven Arild Helleland <sven.helleland@kaizen-web.com>
2022-09-22 22:25:58 +0200

    Increased the line length before the prettify kicks in and modify it.

--- .eslintrc.js
+++ .eslintrc.js
@@ -106,7 +106,7 @@ module.exports = {
       {
         trailingComma: 'es5',
         singleQuote: true,
-        printWidth: 80,
+        printWidth: 150,
         // below line only for windows users facing CLRF and eslint/prettier error
         // non windows users feel free to delete it
         endOfLine: 'auto',
